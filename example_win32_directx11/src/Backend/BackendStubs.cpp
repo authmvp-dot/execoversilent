e3ae60d23066204fb213fe818b956702e5037ea2
authmvp-dot <authmvp-dot@users.noreply.github.com>
2026-08-09 10:36:57 +0530

    Fix 6 linker errors: add BackendStubs.cpp with AimbotMemory Aim, SpinPlayer, FlyHack, NoGravityFly defs

---
+++ example_win32_directx11/src/Backend/BackendStubs.cpp
@@ -0,0 +1,24 @@
+// Stub definitions for linker symbols that have no implementation
+#include <AIMBOTMEMORY.H>
+
+// Global AimbotMemory instance
+AimbotMemory Aim;
+
+// SpinPlayer namespace stubs
+namespace SpinPlayer {
+    void Start() {}
+    void Stop() {}
+}
+
+// FlyHack_LocalPlayer namespace stubs
+namespace FlyHack_LocalPlayer {
+    void Start() {}
+    void Stop() {}
+    void ApplyFly() {}
+}
+
+// NoGravityFly namespace stubs
+namespace NoGravityFly {
+    void Start() {}
+    void Stop() {}
+}
