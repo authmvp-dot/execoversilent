ccd222582444e2c62268cadcec159fa847fd3aa7
authmvp-dot <authmvp-dot@users.noreply.github.com>
2026-08-09 11:02:10 +0530

    Add missing inline symbol definitions to UIStubs.hpp for clean linking

--- example_win32_directx11/UIStubs.hpp
+++ example_win32_directx11/UIStubs.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <functional>
 #include <atomic>
+#include <d3d11.h>
+#include <AIMBOTMEMORY.H>
 
 inline std::atomic<bool> g_AdbReady{ true };
 inline std::atomic<bool> g_AdbFailed{ false };
@@ -26,3 +28,33 @@ inline void SyncUIToGlobals() {}
 inline void Backend_LoadConfig() {}
 inline void Backend_SaveConfig() {}
 inline void Backend_ResetConfig() {}
+
+// Global AimbotMemory instance
+inline AimbotMemory Aim;
+
+// keybind_menu_key
+inline int keybind_menu_key = 0;
+
+// texture::custom_logo
+namespace texture {
+    inline ID3D11ShaderResourceView* custom_logo = nullptr;
+}
+
+// FlyHack_LocalPlayer stubs
+namespace FlyHack_LocalPlayer {
+    inline void Start() {}
+    inline void Stop() {}
+    inline void ApplyFly() {}
+}
+
+// NoGravityFly stubs
+namespace NoGravityFly {
+    inline void Start() {}
+    inline void Stop() {}
+}
+
+// SpinPlayer stubs
+namespace SpinPlayer {
+    inline void Start() {}
+    inline void Stop() {}
+}
