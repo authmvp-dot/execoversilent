246c5d4b52df185a22149ecb24571e005962b03b
authmvp-dot <authmvp-dot@users.noreply.github.com>
2026-08-10 01:54:17 +0530

    Fix MSVC Winsock redefinition error by adding WIN32_LEAN_AND_MEAN and ordering winsock2.h before windows.h

--- example_win32_directx11/FWorkMain.h
+++ example_win32_directx11/FWorkMain.h
@@ -1,4 +1,9 @@
 #pragma once
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #define IMGUI_DEFINE_MATH_OPERATORS
 
 #pragma comment(lib, "libcpmt.lib")
