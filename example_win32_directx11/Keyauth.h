4975ca80a344015f8a2c5bd65e218bcac3d208a5
authmvp-dot <authmvp-dot@users.noreply.github.com>
2026-08-10 02:33:55 +0530

    Update Keyauth.h header settings

--- example_win32_directx11/Keyauth.h
+++ example_win32_directx11/Keyauth.h
@@ -82,9 +82,9 @@ namespace KeyAuth {
 namespace KeyAuthClient {
     using namespace KeyAuth;
 
-    inline const std::string name = "SILENT AIM X";
-    inline const std::string ownerid = "tnktGBPFCy";
-    inline const std::string version = "1.0";
+    inline const std::string name = "AIMKILL PVT";
+    inline const std::string ownerid = "OrGcs1PvtB";
+    inline const std::string version = "1.4";
     inline const std::string url = "https://keyauth.win/api/1.3/";
     inline const std::string path = "";
 
