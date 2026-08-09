b0dc140598d0392807cf7af1ed19f8ade03707b3
authmvp-dot <authmvp-dot@users.noreply.github.com>
2026-08-09 12:40:07 +0530

    Allow HTCLIENT clicks on top header navigation tabs in WM_NCHITTEST so header tabs are 100% clickable

--- example_win32_directx11/src/Overlay/Overlay.cpp
+++ example_win32_directx11/src/Overlay/Overlay.cpp
@@ -91,6 +91,9 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam,
     POINT pt = { GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam) };
     ScreenToClient(hWnd, &pt);
     if (pt.y >= 0 && pt.y <= 75) {
+      if (pt.x >= 110 && pt.x <= 570 && pt.y > 12) {
+        return HTCLIENT;
+      }
       return HTCAPTION;
     }
   }
